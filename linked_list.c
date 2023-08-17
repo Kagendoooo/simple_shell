5640999844b60d04e20fdfae172c905c76f24608
Kamundia-Simon <simonkamundia8@gmail.com>
2023-08-17 20:25:43 +0300

    free list

---
+++ linked_list.c
@@ -0,0 +1,51 @@
+#include "shell.h"
+
+/**
+  *add_node_end - adds a node to the end of the linked list
+  *@head: pointer to head of linked list
+  *@dir: directory for new node
+  *Return: NULL/new node
+  */
+
+list_t *add_node_end(list_t **head, char *dir)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *last;
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->dir = dir;
+	new_node->next = NULL;
+
+	if (*head)
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new_node;
+	}
+	else
+		*head = new_node;
+
+	return (new_node);
+}
+
+/**
+  *free_list - frees linked list
+  *@head: head of linked list
+  */
+
+void free_list(list_t *head)
+{
+	list_t *next;
+
+	while (head)
+	{
+		next = head->next;
+		free(head->dir);
+		free(head);
+		head = next;
+	}
+}
+
