031a829e8a80c31aa1a34125d40b3cb006d94524
Kamundia-Simon <simonkamundia8@gmail.com>
2023-08-18 23:38:02 +0300

    find digit length

--- 0error_handling.c
+++ 0error_handling.c
@@ -29,3 +29,51 @@ int num_len(int num)
 	return (len);
 }
 
+/**
+  *_itoa - converts int to string
+  *@num: integer value
+  *Return: string
+  */
+
+char *_itoa(int num)
+{
+	char *buff;
+	int len = num_len(num);
+	unsigned int x;
+
+	buff = malloc(sizeof(char) * (len + 1));
+	if (!buff)
+		return (NULL);
+	buff[len] = '\0';
+
+	if (num < 0)
+	{
+		x = num * -1;
+		buff[0] = '-';
+
+		else
+		{
+			x = num;
+		}
+
+		len--;
+		do
+		{
+			buff[len] = (x % 10) + '0';
+			x /= 10;
+			len--;
+		}
+		while (x > 0)
+			return (buff);
+	}
+}
+
+/**
+  *create_err - custom error msg to stderr
+  *@args: an array of arguments
+  *@err: the error val
+  *Return: err value
+  */
+int create_err(char **arg, int err)
+{
+	char *error;
