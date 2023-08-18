f6fe3a8db296b6ea80305fbe6669d3f85e07cb58
Kamundia-Simon <simonkamundia8@gmail.com>
2023-08-18 18:07:32 +0300

    util_paths.c

---
+++ util_paths.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+
+/**
+ *get_location - locate a command in path
+ *@command: locate command
+ *Return: NULL/directory
+ */
+char *get_location(char *command)
+{
+	char **path, *tmp;
+	list_t *dirs, *head;
+	struct stat st;
+
+	path = _getenv("PATH");
+	if (!path || !(*path))
+		return (NULL);
+
+	dirs = get_path_dir(*path + 5);
+	head = dirs;
+
+	while (dirs)
+	{
+		tmp = malloc(_strlen(dirs->dir) + _strlen(command) + 2);
+		if (!tmp)
+			return (NULL);
+		_strcpy(tmp, dirs->dir);
+		_strcat(tmp, "/");
+		_strcat(tmp, command);
+
+		if (stat(tmp, &st) == 0)
+		{
+			free_list(head);
+			return (tmp);
+		}
+
+		dirs = dirs->next;
+		free(tmp)
+	}
+	free_list(head);
+	return (NULL);
+}
