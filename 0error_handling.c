dc65bd8b02b8e67ab11f0a1f9c3c226fe913ea04
Kamundia-Simon <simonkamundia8@gmail.com>
2023-08-18 21:02:51 +0300

    find digit length

---
+++ 0error_handling.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+  *num_len - finds digit length of number
+  *@num: integer value
+  *Return: digit's length
+  */
+
+int num_len(int num)
+{
+	unsigned int i;
+	int len = 1;
+
+	if (num < 0)
+	{
+		len++;
+		x = num * -1
+	}
+	else
+	{
+		x = num
+	}
+
+	while (x > 9)
+	{
+		len++;
+		x /= 10;
+	}
+	return (len);
+}
+
