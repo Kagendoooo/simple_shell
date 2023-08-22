9b85cfcc318781e29be7ec53f138dba919ad0eed
Kamundia-Simon <simonkamundia8@gmail.com>
2023-08-23 00:37:12 +0300

    added shellby_cd

--- shellby.c
+++ shellby.c
@@ -10,6 +10,12 @@ int (*_builtin(char *command))(char **args, char **frt)
 {
 	builtin_t funcs[] = {
 		{ "exit", _exit },
+		{ "env", shellby_env },
+		{ "setenv", _setenv },
+		{ "unsetenv", _unsetenv },
+		{ "cd", shellby_cd },
+		{ "alias", shellby_alias },
+		{ "help", shellby_help },
 		{NULL, NULL}
 	};
 	int i;
@@ -23,10 +29,10 @@ int (*_builtin(char *command))(char **args, char **frt)
 }
 
 /**
-  *_exit - process termination
-  *@args: array of arguments
-  *Return: 2 or 0
-  */
+ *_exit - process termination
+ *@args: array of arguments
+ *Return: 2 or 0
+ */
 int _exit(char **args)
 {
 	int i = 0, len_of_int = 10;
@@ -59,4 +65,88 @@ int _exit(char **args)
 	free_alias_list(aliases);
 	exit(num);
 }
+
+/**
+ *shellby_cd - changes current directory
+ *@args: array of arguments
+ *frt: double pointer to args
+ *Return: -2, -1 or 0
+ */
+
+int shellby_cd(char **args, char __attribute__((__unused__)) **frt)
+{
+	char **dir_info, *new_line = "\n";
+	char *oldpwd = NULL, *pwd = NULL;
+	struct stat dir;
+
+	oldpwd = getcwd(oldpwd, 0);
+	if (!oldpwd)
+		return (-1);
+	if (args[0])
+	{
+		if (args[0])
+		{
+			if (*(args[0]) == '-' || _strcmp(args[0], "--") == 0)
+			{
+				if ((args[0][1] == '-' && args[0][2] == '\0') ||
+						args[0][1] == '\0')
+				{
+					if (_getenv("OLDPWD") != NULL)
+						(chdir(*_getenv("OLDPWD") + 7));
+				}
+				else
+				{
+					free(oldpwd);
+					return (create_err(args, 2));
+				}
+			}
+			else
+			{
+				if (stat(args[0], &dir) == 0 && S_ISDIR(dir.st_mode)
+						&& ((dir.st_mode & S_IXUSR) != 0))
+					chdir(args[0]);
+				else
+				{
+					free(oldpwd);
+					return (create_err(args, 2));
+				}
+			}
+		}
+		else
+		{
+			if (_getenv("HOME") != NULL)
+				chdir(*(_getenv("HOME")) + 5);
+		}
+
+
+		pwd = getcwd(pwd, 0);
+		if (!pwd)
+			return (-1);
+
+
+		dir_info = malloc(sizeof(char *) * 2);
+		if (!dir_info)
+			return (-1);
+
+
+		dir_info[0] = "OLDPWD";
+		dir_info[1] = oldpwd;
+		if (_setenv(dir_info, dir_info) == -1)
+			return (-1);
+
+
+		dir_info[0] = "PWD";
+		dir_info[1] = pwd;
+		if (_setenv(dir_info, dir_info) == -1)
+			return (-1);
+		if (args[0] && args[0][0] == '-' && args[0][1] != '-')
+		{
+			write(STDOUT_FILENO, pwd, _strlen(pwd));
+			write(STDOUT_FILENO, new_line, 1);
+		}
+		free(oldpwd);
+		free(pwd);
+		free(dir_info);
+		return (0);
+	}
 
