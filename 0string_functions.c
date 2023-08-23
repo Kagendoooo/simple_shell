f6daa90454e5042d3e434efeac1ecf6e31ca3409
Kagendoooo <kagendoooo@gmail.com>
2023-08-23 23:00:36 +0300

    Added concatenate functions

--- 0string_functions.c
+++ 0string_functions.c
@@ -1,5 +1,7 @@
 #include "shell.h"
 
+int _strlen(const char *s);
+
 /**
  * _strlen - Returns length of string
  * @s: pointer to characters string
@@ -16,12 +18,14 @@ int _strlen(const char *s)
 	return (len);
 }
 
+char *_strcpy(char *dest, const char *src);
+
 /**
  * _strcpy - Copies string
  * @dest: destination
  * @src: source
- * Return: Pointer to dest.
-*/
+ * Return: Pointer to dest
+ */
 char *_strcpy(char *dest, const char *src)
 {
 	size_t x;
@@ -29,5 +33,52 @@ char *_strcpy(char *dest, const char *src)
 	for (x = 0; src[x] != '\0'; x++)
 		dest[x] = src[x];
 	dest[x] = '\0';
+	return (dest);
+}
+
+char *_strcat(char *dest, const char *src);
+
+/**
+ * _strcat - Concantenates two strings
+ * @dest: destination
+ * @src: source
+ * Return: Pointer
+*/
+
+char *_strcat(char *dest, const char *src)
+{
+	char *a;
+	const char *b;
+
+	a = dest;
+	b =  src;
+
+	while (*a != '\0')
+		a++;
+
+	while (*b != '\0')
+		*a++ = *b++;
+	*a = '\0';
+	return (dest);
+}
+
+char *_strncat(char *dest, const char *src, size_t n);
+
+/**
+ * _strncat - Concantenates two strings
+ * @dest: destination
+ * @src: source
+ * @n: bytes to copy
+ * Return: Pointer
+*/
+char *_strncat(char *dest, const char *src, size_t n)
+{
+	size_t dlen = _strlen(dest);
+	size_t z;
+
+	for (z = 0; z < n && src[z] != '\0'; z++)
+		dest[dlen + z] = src[z];
+	dest[dlen + z] = '\0';
+
 	return (dest);
 }
