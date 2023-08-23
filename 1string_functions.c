025bd538b274ea2fdc91d8670617e3315a82677b
Kagendoooo <kagendoooo@gmail.com>
2023-08-24 00:55:11 +0300

    Updated complex string function

--- 1string_functions.c
+++ 1string_functions.c
@@ -1,10 +1,13 @@
 #include "shell.h"
+
+char *_strchr(char *s, char c);
+
 /**
  * _strchr - Locate a character in string
  * @s: string
  * @c: character to be located
  * Return: Pointer or NULL
-*/
+ */
 char *_strchr(char *s, char c)
 {
 	int idx;
@@ -18,12 +21,14 @@ char *_strchr(char *s, char c)
 	return (NULL);
 }
 
+int _strspn(char *s, char *accept);
+
 /**
  * _strspn - Gets the length of substring prefix
  * @s: string
  * @accept: prefix to be measured
  * Return: number of bytes in s
-*/
+ */
 int _strspn(char *s, char *accept)
 {
 	int b = 0;
@@ -42,4 +47,52 @@ int _strspn(char *s, char *accept)
 		s++;
 	}
 	return (b);
+}
+
+int _strcmp(char *s1, char *s2);
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: byte difference
+*/
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 != *s2)
+		return (*s1 - *s2);
+
+	return (0);
+}
+
+int _strncmp(const char *s1, const char *s2, size_t n);
+
+/**
+ * _strncmp - Compare two strings
+ * @s1: Pointer to string 1
+ * @s2: Pointer to string 2
+ * @n: first n bytes of strings to compare
+ * Return: Less than 0 or Greater than 0
+*/
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t x;
+
+	for (x = 0; s1[x] && s2[x] && x < n; x++)
+	{
+		if (s1[x] > s2[x])
+			return (s1[x] - s2[x]);
+		else if (s1[x] < s2[x])
+			return (s1[x] - s2[x]);
+	}
+	if (x == n)
+		return (0);
+	else
+		return (-15);
 }
